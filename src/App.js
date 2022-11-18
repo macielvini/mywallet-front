3dc81c1a093cd803f597eec182d3b39e80d5f310
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-11-18 15:25:31 -0300

    feat: initial setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./assets/css/globalStyles";
+import SignIn from "./pages/SignInPage/SignIn";
+import AuthRoutes from "./routes/AuthRoutes";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalStyle />
+        {/* <Routes>
+          <Route path="/" element={<SignIn />} />
+        </Routes> */}
+        <AuthRoutes />
+      </BrowserRouter>
+    </>
+  );
+}
